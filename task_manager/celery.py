070410fc3a41d64ed92acab4d6543c879d4fb421
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2022-02-01 10:32:08 +0530

    added periodic report emails

---
+++ task_manager/celery.py
@@ -0,0 +1,52 @@
+import os
+from datetime import timedelta
+
+from django.conf import settings
+
+from celery import Celery
+from celery.decorators import periodic_task
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_manager.settings")
+app = Celery("task_manager")
+app.config_from_object("django.conf:settings")
+
+# Periodic Task
+
+
+@periodic_task(run_every=timedelta(seconds=60))
+def batch_email():
+
+    from django.db.models import Q
+    from tasks.models import Report
+    from functools import reduce
+    from collections import defaultdict
+    from tasks.models import Task
+    from django.core.mail import send_mail
+    from django.template.loader import render_to_string
+    from datetime import timedelta, datetime, timezone
+
+    def user_summary(user):
+        def ad(prev, task):
+            prev[task.status] += 1
+            return prev
+
+        tasks = Task.objects.filter(user=user, completed=False, deleted=False)
+        return {
+            "name": user.username.capitalize(),
+            "status": dict(
+                reduce(ad, tasks, defaultdict(int))
+            )
+        }
+
+    start = datetime.now(timezone.utc) - timedelta(days=1)
+
+    for report in Report.objects.filter(Q(last_updated=None) | Q(last_updated__lt=start)):
+        send_mail(
+            "Daily Status Report",
+            render_to_string("report.txt", user_summary(report.user)),
+            "tasks@taskmanager.com",
+            [report.user.email]
+        )
+        report.last_updated = datetime.now(timezone.utc).replace(
+            hour=report.time.hour, second=report.time.second)
+        report.save()
